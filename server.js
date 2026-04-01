5c7ec79cc3009c62d63ee78045157294b611bf05
arivas18 <arivas18@outlook.com>
2026-03-31 23:59:36 -0500

    feat: implement Express server with Mongoose CRUD and REST test suite

--- server.js
+++ server.js
@@ -5,12 +5,13 @@ const Log = require('./models/Log');
 const app = express();
 const PORT = 3000;
 
-// Middleware to let Express understand JSON data
+// 1. MIDDLEWARE to let Express understand JSON data
 app.use(express.json());
 
-// Connect to your MongoDB Atlas cluster
+// Connect to MongoDB Atlas cluster
 connectDB();
 
+// 3. GET ROUTES (Reading data)
 // --- THE "HELLO WORLD" ROUTE ---
 app.get('/', (req, res) => {
     res.send('<h1>Hello World!</h1><p>The Express server is officially live.</p>');
@@ -30,6 +31,22 @@ app.get('/status', async (req, res) => {
     }
 });
 
+// 4. POST ROUTES (Creating data)
+app.post('/logs', async (req, res) => {
+    try {
+        const newLog = new Log({
+            action: req.body.action,
+            category: req.body.category
+        });
+
+        const savedLog = await newLog.save();
+        res.status(201).json(savedLog); // 201 means "Created"
+    } catch (error) {
+        res.status(400).json({ message: "Error saving log", error: error.message });
+    }
+});
+
+// 5. START SERVER
 app.listen(PORT, () => {
     console.log(`🚀 Server is running at http://localhost:${PORT}`);
 });
